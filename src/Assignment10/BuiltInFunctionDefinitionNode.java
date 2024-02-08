7a2e41618972dd4a3e4bd73de142a092f140c355
Zain Ahmad <142930432+ZainAhmad2@users.noreply.github.com>
2024-02-08 13:02:28 -0500

    Update BuiltInFunctionDefinitionNode.java

--- src/Assignment10/BuiltInFunctionDefinitionNode.java
+++ src/Assignment10/BuiltInFunctionDefinitionNode.java
@@ -20,19 +20,3 @@ public class BuiltInFunctionDefinitionNode extends FunctionDefinitionNode {
         return execute.apply(parameters);
     }
 }
-// Failed attempts at getting my parameter to read HAHA
-   /* public LinkedList<String> getParameters(LinkedList<String> parameters) {
-        LinkedList<String> parameterList = new LinkedList<>();
-        for (String parameterName : parametersMap.keySet()) {
-            parameterList.add(parameterName);
-        }
-        return parameterList;
-        public BuiltInFunctionDefinitionNode(HashMap<String, InterpreterDataType> parametersMap){
-        this.parametersMap = parametersMap;
-    }
-    }
-    //import java.util.Arrays;
-    //import java.util.LinkedList;
-    //import java.util.function.Function;
-    //import java.util.regex.Matcher;
-    //import java.util.regex.Pattern;*/
